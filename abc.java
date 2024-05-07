48fe1a5e318f86f724cc51248dd19798907b3300
Utkarsh Mishra <100478740+tkarshmishra@users.noreply.github.com>
2024-05-07 14:20:58 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,23 @@
+ub1ic class Weatherl•'oreca st-App {
+    public static void main (String [l args) {
+    JFrame ("Weather Forecast App") ;
+    JFrame Lrarne —
+    new
+    frame . setDefau1tC10seOperation (JFrame . EXIT ON CLOSE) ;
+    frame. set-Si ze (400, 300) ;
+    L rame . seLLayouL (new
+    FlowLayouL ( ) ) ;
+    J'l'extl•'i eld locati onl•'ield
+    J'l'ext-l•'i eld (15) ;
+    new
+    new JBuLLon ("Fetch WeaLher") ;
+    JBuLLon LeLchBuL Lon —
+    JTextArea weatherDisplay
+    JTextArea (10, 30) ;
+    new
+    weatherl)i spl a y. tabl e (false) ;
+    frame . add (new JLabe1 ("Enter City Name") ) ;
+    frame . add (l ocati onl•'ield) ;
+    L rame . add (LeLchBuL Lon) ;
+    frame . set Visible (true) ;
+    1
